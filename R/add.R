3c65bebccc4c079534e6027df70b5af9b3314e11
JieYinStat <yjabcdhot@hotmail.com>
2024-02-12 10:24:38 +0800

    test

--- R/add.R
+++ R/add.R
@@ -9,3 +9,16 @@
 add <- function(x, y) {
   x + y
 }
+
+# new function
+add4 <- function(x, y, z) {
+  x + y + z
+}
+
+# old function
+#' @description
+#' `r lifecycle::badge("deprecated")`
+plus3 <- function(x, y, z) {
+  lifecycle::deprecate_warn("1.0.0", "plus3()", "add4()")
+  add4(x, y, z)
+}
--- R/regexcite-package.R
+++ R/regexcite-package.R
@@ -4,7 +4,8 @@
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
-#' @importFrom glue glue_collapse
 #' @import rlang
+#' @importFrom glue glue_collapse
+#' @importFrom lifecycle deprecated
 ## usethis namespace: end
 NULL
